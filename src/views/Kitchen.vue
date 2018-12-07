7ab3876b54e1f9fb3c25a9c86894a184dc1d7257
CJCCarlberg <cj@castencarlberg,se>
2018-12-07 18:27:11 +0100

    Kitchen: tried to get order, timer and button within the same div

--- src/views/Kitchen.vue
+++ src/views/Kitchen.vue
@@ -1,20 +1,13 @@
 <template>
   <div class="root">
 
-    <div class="homePage" v-show = "page===0">  <!--Home page for staff -->
-      <StaffHomePage
-      v-on:workFlow="changePage(1)"
-      v-on:saldo="changePage(2)"
-      v-on:statistics="changePage(3)"
-      :ui-labels="uiLabels"
-      :lang="lang"
-      >
-      </StaffHomePage>
-    </div>
 
+    <div v-show = "page===0">  <!--Home page for staff -->
+      <button v-on:click="changePage(1)">Go to work flow</button>
+
+    </div>
     <div v-show = "page===1">  <!--Work flow for orders-->
       <div id="orders">
-
         <h1>{{ uiLabels.ordersInQueue }}</h1>
         <div id="ordersToPrepare">
           <OrderItemToPrepare class="singleOrder"
@@ -28,9 +21,10 @@
             :key="key">
           </OrderItemToPrepare>
         </div>
+
         <h1>{{ uiLabels.ordersWorkingOn }}</h1>
         <div id="ordersWorkedOn">
-          <OrderItemBeingPrepared ref="timer" class="singleOrder"
+          <OrderItemBeingPrepared class="singleOrder"
             v-for="(order, key) in orders"
             v-if="order.status === 'started'"
             v-on:done="markDone(key)"
@@ -41,6 +35,7 @@
             :key="key">
           </OrderItemBeingPrepared>
         </div>
+
         <h1>{{ uiLabels.ordersFinished }}</h1>
         <div id="finishedOrders">
           <OrderItemDone class="singleOrder"
@@ -54,34 +49,13 @@
             :key="key">
           </OrderItemDone> <!-- orders is found in sharedVueStuff.js -->
         </div>
-        <label>
-          <button class="backButton" v-on:click="changePage(0)">
-            {{uiLabels.back}}
-          </button>
-        </label>
       </div>
     </div>
-
     <div v-show = "page===2">  <!--Saldo -->
 
-
-
-      <label>
-        <button class="backButton" v-on:click="changePage(0)">
-          {{uiLabels.back}}
-        </button>
-      </label>
     </div>
-
     <div v-show = "page===3">  <!--Product statistics -->
 
-
-
-      <label>
-        <button class="backButton" v-on:click="changePage(0)">
-          {{uiLabels.back}}
-        </button>
-      </label>
     </div>
 
   </div>
@@ -91,8 +65,6 @@ import OrderItem from '@/components/OrderItem.vue'
 import OrderItemToPrepare from '@/components/OrderItemToPrepare.vue'
 import OrderItemBeingPrepared from '@/components/OrderItemBeingPrepared.vue'
 import OrderItemDone from '@/components/OrderItemDone.vue'
-import StaffHomePage from '@/components/StaffHomePage.vue'
-import Timer from '@/components/Timer.vue'
 
 //import methods and data that are shared between ordering and kitchen views
 import sharedVueStuff from '@/components/sharedVueStuff.js'
@@ -103,9 +75,7 @@ export default {
     OrderItem,
     OrderItemToPrepare,
     OrderItemBeingPrepared,
-    OrderItemDone,
-    StaffHomePage,
-    Timer
+    OrderItemDone
   },
   mixins: [sharedVueStuff], // include stuff that is used in both
                             //the ordering system and the kitchen
@@ -121,8 +91,7 @@ export default {
       this.$store.state.socket.emit("orderDone", orderid);
     },
     markStarted: function (orderid) {
-      this.$refs.timer.startTimer();
-      //this.$store.state.socket.emit("orderStarted", orderid)
+      this.$store.state.socket.emit("orderStarted", orderid)
     },
     markServed: function (orderid) {
       this.$store.state.socket.emit("orderServed", orderid)
@@ -144,6 +113,8 @@ export default {
     font-size: 24pt;
     grid-row-gap: 1em;
     grid-column-gap: .5em;
+    min-height:100%;
+    max-height:100%;
   }
 
   #ordersToPrepare{
@@ -151,33 +122,22 @@ export default {
     grid-column-start: 1;
     grid-column-end: 1;
     grid-row-start: 2;
-    grid-template-columns: repeat(1,1fr);
-    min-height:100%;
-    max-height:100%;
+    grid-template-columns: repeat(1, minmax(1em 1fr));
     /*overflow-y: auto;*/
   }
   .singleOrder{ /*Class for all orders*/
     border: 2px solid;
     border-color: Crimson;
-    border-radius: 10px;
+    border-radius: 15px;
     height: 3.5em;
   }
-  .homePage {
 
-  }
-  .backButton {
-    position:absolute;
-    top:0;
-    right:0;
-  }
   #ordersWorkedOn{
     display: grid;
     grid-column-start: 2;
     grid-column-end: 2;
     grid-row-start: 2;
-    grid-template-columns: repeat(1,1fr);
-    min-height:100%;
-    max-height:100%;
+    grid-template-columns: repeat(1, minmax(1em 1fr));
     overflow-y: auto;
 
   }
@@ -186,9 +146,7 @@ export default {
     grid-column-start: 3;
     grid-column-end: 3;
     grid-row-start: 2;
-    grid-template-columns: repeat(1,1fr);
-    min-height:100%;
-    max-height:100%;
+    grid-template-columns: repeat(1, minmax(1em 1fr));
     overflow-y: auto;
   }
 
