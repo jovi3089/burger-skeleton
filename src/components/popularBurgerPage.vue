c345d2255e034eed39d6de99c23984aa53119272
erikjsun <erik.j.sun@gmail.com>
2019-01-11 11:11:56 +0100

    CustomerView: Added new components, for realz

---
+++ src/components/popularBurgerPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <div class = "hey">
+        <p>Hello<p/>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'StartingPage',
+
+  props: {
+    uiLabels: Object,
+    lang: String,
+    step: Number,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/randomBurgerPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <div class = "random">
+        <p>Something Random<p/>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'StartingPage',
+
+  props: {
+    uiLabels: Object,
+    lang: String,
+    step: Number,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
